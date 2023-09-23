99a4417f1d5eff80098be3aaf558d33d57be4ddc
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-09-23 12:49:06 +0530

    added routes and connection files

--- server/index.js
+++ server/index.js
@@ -13,5 +13,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
 
+app.use('/api', )
 app.get('/', (req, res) => res.send('Server is ready'))
 app.listen(port, () => console.log(`Server started on port: ${port}`));
